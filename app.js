cb1e1b599e52c23b79e5f648f8a98e410f73ab9d
Bartek-B <bartlomiej.banach94@gmail.com>
2023-04-24 07:48:31 +0200

    Define Calculator object.

--- app.js
+++ app.js
@@ -1,43 +1,44 @@
+
 // Define Calculator object.
 const Calculator = {
 
     // Define the functions for each operation.
-    add: function(a, b) {
+    add: function (a, b) {
         return (new Decimal(a).plus(b)).toString();
-    }, 
-    subtract: function(a, b) {
+    },
+    subtract: function (a, b) {
         return (new Decimal(a).minus(b)).toString();
-    }, 
-    multiply: function(a, b) {
+    },
+    multiply: function (a, b) {
         return (new Decimal(a).times(b)).toString();
-    }, 
-    divide: function(a, b) {
+    },
+    divide: function (a, b) {
         return (new Decimal(a).dividedBy(b)).toString();
     },
 
     // Define a method to calculate the result.
-    calculate: function(input) {
+    calculate: function (input1, operation, input2) {
         let result;
 
         switch (operation) {
             case '+':
-            result = this.add(input1, input2);
-            break;
+                result = this.add(input1, input2);
+                break;
 
             case '-':
-            result = this.subtract(input1, input2);
-            break;
+                result = this.subtract(input1, input2);
+                break;
 
             case '*':
-            result = this.multiply(input1, input2);
-            break;
+                result = this.multiply(input1, input2);
+                break;
 
             case '/':
-            result = this.divide(input1, input2);
-            break;
+                result = this.divide(input1, input2);
+                break;
         }
 
-        return result.toString()
+        return result;
     }
 }
 
